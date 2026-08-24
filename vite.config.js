3541e60c7af365db1d23f6ef363fea36a838fedf
Klabauter86 <apps@klabauter86.de>
2026-08-24 17:03:07 +0200

    Remove Three.js and keep MapLibre terrain only

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/maplibre-three-gis/',
-  input: {
-    app: 'index.html',
-    threeTest: 'three-test.html',
-  },
 });
